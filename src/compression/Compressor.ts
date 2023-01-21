6199daf91ebdc3d136546a34893ec50f8298c9b0
Jack Le Hamster <jacklehamster@gmail.com>
2023-01-22 07:14:06 +0900

    compression WIP.

---
+++ src/compression/Compressor.ts
@@ -0,0 +1,8 @@
+import { DataStore } from "../reducer/Reducer";
+import { StreamDataView } from "stream-data-view";
+
+export default class Compressor {
+    compress(dataStore: DataStore): ArrayBuffer {
+        const streamDataView = new StreamDataView();
+    }
+}
